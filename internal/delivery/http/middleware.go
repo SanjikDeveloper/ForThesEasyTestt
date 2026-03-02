02a87afb7a5db8b4788ec4f052d5f04251534485
SanjikDeveloper <artha6737@gmail.com>
2026-03-02 15:30:43 +0500

    initial commit

---
+++ internal/delivery/http/middleware.go
@@ -0,0 +1,12 @@
+package http
+
+import (
+	"net/http"
+)
+
+func FilterMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		next.ServeHTTP(w, r)
+	})
+}
