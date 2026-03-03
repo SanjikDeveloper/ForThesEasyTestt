b0b570086d0a737bde9d35284e0494da386b219b
Mekhdiyev Turaj <mehdiev.turadzh@wb.ru>
2026-03-03 11:09:17 +0500

    feat: add todos

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -2,8 +2,12 @@ package models
 
 import "time"
 
+// TODO: зачем тут три параметра используют указатель?
 type Todo struct {
-	IdList      int        `json:"id_list"`
+	// TODO: тут итак понятно чье это айди, не надо писать IdList
+	// TODO: Переменные которые названы аббреиватурно, надо писать все с заглавной буквы. Пример: ID, URL
+	IdList int `json:"id_list"`
+	// TODO: итак понятно, что за лист, в названии не надо слово Todo
 	TodoList    *string    `json:"todo_list"`
 	Description *string    `json:"description"`
 	CreatedAt   *time.Time `json:"created_at"`
