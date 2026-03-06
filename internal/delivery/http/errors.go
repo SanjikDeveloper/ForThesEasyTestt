753157d5f2933f4cacc312310572a18c70dda4a3
SanjikDeveloper <artha6737@gmail.com>
2026-03-07 01:04:02 +0500

    moving router to http + completing TODO's in http directory

--- internal/delivery/http/errors.go
+++ internal/delivery/http/errors.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-// TODO: почему функцию ты сделал экспортируемой? Она где-то будет вне этого пакета использоваться?
-// TODO: клиенту не надо отдавать сообщение с ошибкой. Сделай нормальную обработку сообщений в зависимости от типа ошибки
-// анврапаешь ошибку и в зависимости какая ошибка будет, такой статус ей присваиваешь и решаешь, логировать или нет
-func ErrorResponse(w http.ResponseWriter, status int, message string) {
+func errorResponse(w http.ResponseWriter, status int, message string) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-	// TODO: почему ошибку не обработал?
-	json.NewEncoder(w).Encode(map[string]string{"error": message})
-
+	if err := json.NewEncoder(w).Encode(map[string]string{"error": message}); err != nil {
+		// If we can't even send the error response, there's not much we can do
+		// but at least we tried to handle the error.
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
 }
