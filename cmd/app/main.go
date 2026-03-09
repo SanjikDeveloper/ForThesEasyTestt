d4116c692a2d8f8229f2ac934e5087673350bad8
SanjikDeveloper <artha6737@gmail.com>
2026-03-09 23:05:11 +0500

    adding logger and completing some todos

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,41 +9,43 @@ import (
 	delivery "theSone/internal/delivery/http"
 	"theSone/internal/repository/postgres"
 	"theSone/pkg"
+	logger "theSone/pkg/logger"
+
 	// TODO: поменя на pgx/v5
 	_ "github.com/lib/pq"
 )
 
-// TODO: сделай в pkg отдельный пекейдж с логгером, который ты в мейнике будешь инициализировать и во все слои прокидывать
 func main() {
 	cfg, err := pkg.ReadConfig()
 	if err != nil {
-		slog.Error("error loading config: %s", err.Error())
+		slog.Error("error loading config", "error", err.Error())
 		return
 	}
+	log := logger.NewLogger(&cfg.Logger)
 	//TODO: вынеси в репозиторий
 	dbURL := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+		cfg.Repo.DBHost, cfg.Repo.DBPort, cfg.Repo.DBUser, cfg.Repo.DBPassword, cfg.Repo.DBName)
 
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		slog.Error("error opening db: %s", err.Error())
+		slog.Error("error opening db", "error", err.Error())
 		return
 	}
 	defer db.Close()
 
 	if err := db.Ping(); err != nil {
-		slog.Error("error pinging db: %s", err.Error())
+		slog.Error("error pinging db", "error", err.Error())
 		return
 	}
 
 	repo := postgres.NewTodoRepository(db)
-	app := application.NewApplication(repo)
+	app := application.NewApplication(repo, log)
 	handler := delivery.NewTodoHandler(app)
 
 	mux := delivery.RegisterRoutes(handler)
 
-	fmt.Printf("Server is running on %s\n", cfg.ServerPort)
-	if err := http.ListenAndServe(cfg.ServerPort, mux); err != nil {
-		slog.Error("server error: %s", err.Error())
+	fmt.Printf("Server is running on %s\n", cfg.App.ServerPort)
+	if err := http.ListenAndServe(cfg.App.ServerPort, mux); err != nil {
+		slog.Error("server error", "error", err.Error())
 	}
 }
