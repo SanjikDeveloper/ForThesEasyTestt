91e6e44cec941d08875054a3583461981d3ef898
SanjikDeveloper <artha6737@gmail.com>
2026-03-10 11:23:13 +0500

    completing All TODO's(changing datebase place to Repository and some little corrections)

--- internal/delivery/http/middleware.go
+++ internal/delivery/http/middleware.go
@@ -1,12 +1,1 @@
 package http
-
-import (
-	"net/http"
-)
-
-// TODO: для чего мидлвейр?
-func FilterMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		next.ServeHTTP(w, r)
-	})
-}
