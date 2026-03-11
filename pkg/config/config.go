18eec1e9995688391526b098313871ebc8880939
SanjikDeveloper <artha6737@gmail.com>
2026-03-12 02:39:39 +0500

    adding new error handler and also fixing config bugs

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,13 +7,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func ReadConfig() (*cmd.Config, error) {
+func ReadConfig(cfg any) error {
 	if err := godotenv.Load(); err != nil {
 		slog.Warn(".env file not found, using environment variables", "error", err.Error())
 	}
-	cfg := &Config{}
 	if err := env.Parse(cfg); err != nil {
-		return nil, err
+		return err
 	}
-	return cfg, nil
+	return nil
 }
