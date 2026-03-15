ab94983b49037919786c2932b7e86c99eed3702e
SanjikDeveloper <artha6737@gmail.com>
2026-03-15 23:21:56 +0500

    changing the way handling errors

--- internal/delivery/http/errors.go
+++ internal/delivery/http/errors.go
@@ -3,7 +3,6 @@ package http
 import (
 	"database/sql"
 	"errors"
-	"log"
 	"theSone/internal/models"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,7 +15,8 @@ func (h *TodoHandler) errorResponse(c *fiber.Ctx, err error) error {
 
 	var fiberErr *fiber.Error
 	if errors.As(err, &fiberErr) {
-		return c.Status(fiberErr.Code).JSON(fiber.Map{})
+		h.logger.Error("Fiber error", "code", fiberErr.Code, "err", err)
+		return c.SendStatus(fiberErr.Code)
 	}
 
 	var status int
@@ -25,30 +25,21 @@ func (h *TodoHandler) errorResponse(c *fiber.Ctx, err error) error {
 	switch {
 	case errors.Is(err, models.ErrNotFound), errors.Is(err, sql.ErrNoRows):
 		status = fiber.StatusNotFound
-		message = "Todo not found"
 	case errors.Is(err, models.ErrInvalidInput):
 		status = fiber.StatusBadRequest
 		message = "Invalid input data"
 	case errors.Is(err, models.ErrUnauthorized):
 		status = fiber.StatusUnauthorized
-		message = "Unauthorized"
 	default:
 		status = fiber.StatusInternalServerError
-		message = "Internal server error"
-
-		if h.app != nil {
-			logInternalError(err)
-		}
+		h.logger.Error("Internal server error")
 	}
-	// TODO: пользователям не надо видеть что за ошибка на беке. Ее главное залогировать и отдать 500 статус
+
+	if message == "" {
+		return c.SendStatus(status)
+	}
+
 	return c.Status(status).JSON(fiber.Map{
 		"error": message,
 	})
-}
-
-// TODO: сделай данную функуцию методом handler и у нее используй общий логгер для сервиса. Везде где нужно что-то логировать
-// прокидывай общий логгер и его используй
-func logInternalError(err error) {
-
-	log.Printf("Internal error: %+v", err)
 }
